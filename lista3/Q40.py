cb89a44cdaf35d1f944c247facdb43697039704b
AlexandrePeBrito <alexandre_pereirabrito@yahoo.com.br>
2021-08-07 11:21:38 -0300

    atividades

--- lista3/Q40.py
+++ lista3/Q40.py
@@ -7,3 +7,43 @@
 #d. Qual a média de veículos nas cinco cidades juntas;
 #e. Qual a média de acidentes de trânsito nas cidades com menos de 2.000 veículos de
 #passeio.
+
+cidades={}
+cod=1
+num_veic=int(input(f"Informe o numeros de veiculos(em 1999) da cidade {cod}: "))
+num_aci=int(input(f"Informe o numero de acidentes(em 1999) da cidade {cod}: "))
+veic=[]
+acid=[]
+menor=[]
+maior=[]
+
+municipios={cod:(num_veic,num_aci)}
+cidades.update(municipios)
+
+for c in range(1,5):
+    cod+=1
+    veic.append(num_veic)
+    acid.append(num_aci)
+    num_veic=int(input(f"\nInforme o numeros de veiculos(em 1999) da cidade {cod}: "))
+    num_aci=int(input(f"\nInforme o numero de acidentes(em 1999) da cidade {cod}: "))
+    municipios={cod:(num_veic,num_aci)}
+    cidades.update(municipios)
+
+media_veic=sum(veic)/5
+media_aci=sum(acid)/5
+
+menor.append(cidades[1][1])
+menor.append(0)  
+maior.append(cidades[1][1])
+maior.append(0)  
+
+for c in range(1,len(cidades)+1):
+    if(cidades[c][1]<menor[0]):
+        menor[0]=cidades[c][1]
+        menor[1]=c
+    if(cidades[c][1]>maior[0]):
+        maior[0]=cidades[c][1]
+        maior[1]=c
+    
+print(f"A cidade com o menor indice de acidentes tem o codigo {menor[1]} tendo {menor[0]} acidentes")
+print(f"A cidade com o maior indice de acidentes tem o codigo {maior[1]} tendo {maior[0]} acidentes")   
--- lista3/Q42.py
+++ lista3/Q42.py
@@ -1,3 +1,30 @@
 #Faça um programa que leia uma quantidade indeterminada de números positivos e conte
 #quantos deles estão nos seguintes intervalos: [0-25], [26-50], [51-75] e [76-100]. A entrada de
 #dados deverá terminar quando for lido um número negativo.
+
+num=int(input("Informe um numero: "))
+numeros=[]
+cl1=0
+cl2=0
+cl3=0
+cl4=0
+#clas[0]=[0-25]
+#clas[1]=[26-50]
+#clas[2]=[51-75]
+#clas[3]=[76-100]
+
+while(num>=0):
+    numeros.append(num)
+    num=int(input("\nInforme um numero: "))
+
+for c in range(0,len(numeros)):
+    if(numeros[c]>=0 and numeros[c]<=25):
+        cl1+=1
+    elif(numeros[c]>=26 and numeros[c]<=50):
+        cl2+=1
+    elif(numeros[c]>=51 and numeros[c]<=75):
+        cl3+=1
+    elif(numeros[c]>=76 and numeros[c]<=100):
+        cl4+=1
+    
+print(f"\nDentre os numeros digitados tem {cl1} numeros entre [0-25], {cl2} numeros entre [26-50], {cl3} numeros entre [51-75], {cl4} numeros entre [76-100]")
