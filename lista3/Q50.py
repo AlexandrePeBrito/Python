77e92bd7ee8f245e637c454142463206d0fb4ef1
AlexandrePeBrito <alexandre_pereirabrito@yahoo.com.br>
2021-08-09 11:14:28 -0300

    atividade

--- lista3/Q50.py
+++ lista3/Q50.py
@@ -1,2 +1,29 @@
 #Sendo H= 1 + 1/2 + 1/3 + 1/4 + ... + 1/N, Faça um programa que calcule o valor de H com N
 #termos.
+
+n=int(input("Informe o termo N que deseja visualizar: "))
+serie=[]
+serie_attN=[]
+serie_attV=[]
+cima=1
+baixo=1
+serie.append(cima)
+serie.append(baixo)
+
+for c in range(1,n):
+    baixo+=1
+    serie.append(cima)
+    serie.append(baixo)
+txt=f"{str(serie[0])}"
+num=serie[0]/serie[1]
+serie_attN.append(num)
+serie_attV.append(txt)
+for c in range(2,n*2,2):
+    num=serie[c]/serie[c+1]
+    txt=f"+ {str(serie[c])}/{str(serie[c+1])}"
+    serie_attN.append(num)
+    serie_attV.append(txt)
+
+print(f"H = {serie_attV} = {round(sum(serie_attN),2)}")
+    
+
