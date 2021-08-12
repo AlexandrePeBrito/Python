a22c7be933aba820546d37b97af268161cb81843
AlexandrePeBrito <alexandre_pereirabrito@yahoo.com.br>
2021-08-11 23:14:56 -0300

    atividade

---
+++ lista4/Q17.py
@@ -0,0 +1,16 @@
+Em uma competição de salto em distância cada atleta tem direito a cinco saltos. O resultado do
+atleta será determinado pela média dos cinco valores restantes. Você deve fazer um programa
+que receba o nome e as cinco distâncias alcançadas pelo atleta em seus saltos e depois informe
+o nome, os saltos e a média dos saltos. O programa deve ser encerrado quando não for
+informado o nome do atleta. A saída do programa deve ser conforme o exemplo abaixo:
+18.Atleta: Rodrigo Curvêllo
+19.
+20.Primeiro Salto: 6.5 m
+21.Segundo Salto: 6.1 m
+22.Terceiro Salto: 6.2 m
+23.Quarto Salto: 5.4 m
+24.Quinto Salto: 5.3 m
+25.
+26.Resultado final:
+27.Atleta: Rodrigo Curvêllo
+28.Saltos: 6.5 - 6.1 - 6.2 - 5.4
---
+++ lista4/Q19.py
@@ -0,0 +1,31 @@
+Uma empresa de pesquisas precisa tabular os resultados da seguinte enquete feita a uma
+grande quantidade de organizações:
+53."Qual o melhor Sistema Operacional para uso em servidores?"
+54.
+55.As possíveis respostas são:
+56.
+57.1- Windows Server
+58.2- Unix
+59.3- Linux
+60.4- Netware
+61.5- Mac OS
+6- Outro
+Você foi contratado para desenvolver um programa que leia o resultado da enquete e informe ao
+final o resultado da mesma. O programa deverá ler os valores até ser informado o valor 0, que
+encerra a entrada dos dados. Não deverão ser aceitos valores além dos válidos para o programa
+(0 a 6). Os valores referentes a cada uma das opções devem ser armazenados num vetor. Após
+os dados terem sido completamente informados, o programa deverá calcular a percentual de
+cada um dos concorrentes e informar o vencedor da enquete. O formato da saída foi dado pela
+empresa, e é o seguinte:
+Sistema Operacional Votos %
+------------------- ----- ---
+Windows Server 1500 17%
+Unix 3500 40%
+Linux 3000 34%
+Netware 500 5%
+Mac OS 150 2%
+Outro 150 2%
+------------------- -----
+Total 8800
+O Sistema Operacional mais votado foi o Unix, com 3500 votos,
+correspondendo a 40% dos votos.
